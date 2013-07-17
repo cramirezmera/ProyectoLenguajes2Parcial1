e831e99485d318c7f8e5ee772c5ca569f5be3df4
Jose <vlezgmezjosantoniomgoo@hotmail.com>
2013-07-17 17:20:39 -0500

    Proyecto de Sudoku en Eclipse usando PYTHON

---
+++ pySudoku/codigoPython/Principal.py
@@ -0,0 +1,20 @@
+'''
+Created on 15/07/2013
+
+@author: josanvel
+'''
+import sys
+from PyQt4 import QtGui
+from VentanaPrincipal import Ui_Principal
+
+def main():
+    aplicacion = QtGui.QApplication(sys.argv)
+    #aplicacion.setStyle("fusion")
+    Principal = QtGui.QMainWindow()
+    ui = Ui_Principal()
+    ui.setupUi(Principal)
+    Principal.show()
+    sys.exit(aplicacion.exec_())
+
+if __name__ == '__main__':
+    main()
